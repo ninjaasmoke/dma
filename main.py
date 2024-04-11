243a67bf89e07f21952093e303ab1479011ec49b
for <nithins674@gmail.com>
2024-04-11 18:34:08 +0100

    Add models and classifiers for logistic regression and naive Bayes

--- main.py
+++ main.py
@@ -11,11 +11,27 @@ from fastapi.responses import HTMLResponse, FileResponse
 import uvicorn
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import requests
+from io import BytesIO
+
 
 import nltk
 nltk.download("stopwords")
 nltk.download("punkt")
 nltk.download("wordnet")
+
+# Lemmatization
+lemmatizer = WordNetLemmatizer()
+
+def load_from_url(url):
+    try:
+        # Fetch the content from the URL
+        response = requests.get(url)
+        response.raise_for_status()
+        return joblib.load(BytesIO(response.content))
+    except requests.exceptions.RequestException as e:
+        print("Error fetching data from URL:", e)
+        return None
 
 
 # Define a comprehensive text preprocessing function
@@ -30,18 +46,23 @@ def preprocess_text(text):
     stop_words = set(stopwords.words("english"))
     words = word_tokenize(text)
     text = " ".join([word for word in words if word not in stop_words])
-    # Lemmatization
-    lemmatizer = WordNetLemmatizer()
     words = word_tokenize(text)
     text = " ".join([lemmatizer.lemmatize(word) for word in words])
     return text
 
 
 # Load the model
-model = joblib.load("logistic_regression_model.joblib")
+log_model_tfidf = joblib.load("logistic_regression_model.joblib")
+log_model_ngrams = load_from_url("https://github.com/ninjaasmoke/data-mining/releases/download/some-tag/logistic_regression_model_ngrams.joblib")
+
+nb_model_tfidf = joblib.load("naive_bayes_model_tfidf.joblib")
+nb_model_ngrams = load_from_url("https://github.com/ninjaasmoke/data-mining/releases/download/some-tag/naive_bayes_model_ngrams.joblib")
+
 
 # Vectorize text using TF-IDF
-vectorizer = joblib.load('tfidf_vectorizer.joblib')
+vectorizer_tfidf = joblib.load('tfidf_vectorizer.joblib')
+vectorizer_ngrams = load_from_url("https://github.com/ninjaasmoke/data-mining/releases/download/some-tag/vectorizer_ngram.joblib")
+
 
 # Initialize FastAPI app
 app = FastAPI()
@@ -67,13 +88,55 @@ async def classify_text(request: TextRequest):
     # Preprocess text
     preprocessed_text = preprocess_text(request.text)
     # Vectorize text
-    vectorized_text = vectorizer.transform([preprocessed_text])
+    vectorized_text = vectorizer_tfidf.transform([preprocessed_text])
     # Predict
-    prediction = model.predict(vectorized_text)
+    prediction = log_model_tfidf.predict(vectorized_text)
     # Map prediction to class label
     class_label = "AI-generated" if prediction[0] == 1 else "Human-written"
     return {"prediction": class_label}
 
+
+@app.post("/logisticClassifierNgrams")
+async def classify_text(request: TextRequest):
+    print("Classifying text...")
+    print(request.text)
+    # Preprocess text
+    preprocessed_text = preprocess_text(request.text)
+    # Vectorize text
+    vectorized_text = vectorizer_ngrams.transform([preprocessed_text])
+    # Predict
+    prediction = log_model_ngrams.predict(vectorized_text)
+    # Map prediction to class label
+    class_label = "AI-generated" if prediction[0] == 1 else "Human-written"
+    return {"prediction": class_label}
+
+@app.post("/naiveBayesClassifier")
+async def classify_text(request: TextRequest):
+    print("Classifying text...")
+    print(request.text)
+    # Preprocess text
+    preprocessed_text = preprocess_text(request.text)
+    # Vectorize text
+    vectorized_text = vectorizer_tfidf.transform([preprocessed_text])
+    # Predict
+    prediction = nb_model_tfidf.predict(vectorized_text)
+    # Map prediction to class label
+    class_label = "AI-generated" if prediction[0] == 1 else "Human-written"
+    return {"prediction": class_label}
+
+@app.post("/naiveBayesClassifierNgrams")
+async def classify_text(request: TextRequest):
+    print("Classifying text...")
+    print(request.text)
+    # Preprocess text
+    preprocessed_text = preprocess_text(request.text)
+    # Vectorize text
+    vectorized_text = vectorizer_ngrams.transform([preprocessed_text])
+    # Predict
+    prediction = nb_model_ngrams.predict(vectorized_text)
+    # Map prediction to class label
+    class_label = "AI-generated" if prediction[0] == 1 else "Human-written"
+    return {"prediction": class_label}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
